68bd9ece42c5a06077da09f5ece82293d648a3da
SukhmanSra <ssra5@uwo.ca>
2025-07-31 15:36:43 -0400

    adding schedule to cpi pipeline.

--- boc_transformer/repository.py
+++ boc_transformer/repository.py
@@ -3,7 +3,7 @@ from dagster import job, op, Definitions
 from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
 from .assets import daily_policy_rate, daily_cpi
 from .resources import fred_api, boc_api
-from .schedules import daily_policy_rate_schedule, daily_policy_rate_job
+from .schedules import daily_policy_rate_schedule, daily_policy_rate_job, daily_cpi_job, daily_cpi_schedule
 
 io_manager = s3_pickle_io_manager.configured({
     "s3_bucket":       {"env": "S3_BUCKET"},
@@ -26,10 +26,10 @@ def hello_world():
 def demo_job():
     hello_world()
 
-defs = Definitions(jobs=[demo_job, daily_policy_rate_job], assets=[daily_policy_rate, daily_cpi],
+defs = Definitions(jobs=[demo_job, daily_policy_rate_job, daily_cpi_job], assets=[daily_policy_rate, daily_cpi],
     resources={
         "s3": s3,
         "io_manager": io_manager,
         "boc_api": boc_api.configured({"base_url": "https://www.bankofcanada.ca/valet"}),
         "fred_api": fred_api.configured({"api_key": os.getenv("FRED_API_KEY")})
-}, schedules=[daily_policy_rate_schedule])
+}, schedules=[daily_policy_rate_schedule, daily_cpi_schedule])
--- boc_transformer/schedules.py
+++ boc_transformer/schedules.py
@@ -6,7 +6,12 @@ from dagster import (
 
 daily_policy_rate_job = define_asset_job(
     name="materialize_daily_policy_rate",
-    selection=AssetSelection.keys("daily_policy_rate"),
+    selection=AssetSelection.keys("daily_policy_rate")
+)
+
+daily_cpi_job = define_asset_job(
+    name="materialize_daily_cpi",
+    selection=AssetSelection.keys("daily_cpi")
 )
 
 daily_policy_rate_schedule = ScheduleDefinition(
@@ -14,4 +19,11 @@ daily_policy_rate_schedule = ScheduleDefinition(
     cron_schedule="15 6 * * *",
     execution_timezone="America/Toronto",
     job=daily_policy_rate_job
+)
+
+daily_cpi_schedule = ScheduleDefinition(
+    name="daily_cpi_schedule",
+    cron_schedule="15 7 * * *",
+    execution_timezone="America/Toronto",
+    job=daily_cpi_job
 )
